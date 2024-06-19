4ada8043427003cd4d4d244652d609f9c5a52546
Jackson Hagen <jackson.d.hagen@gmail.com>
2024-06-18 21:13:26 -0600

    daily challenge pangram

---
+++ Pangram/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Welcome to Pangram Checker!\n\nEnter the phrase you would like to validate:");
+
+string input = Console.ReadLine() ?? String.Empty;
+
+if (IsPangram(input))
+{ }
+
+bool IsPangram(string phrase)
+{
+    if (input.Length < 26)
+    {
+        return false;
+    }
+
+    var letters = new HashSet<char>();
+
+    foreach (char c in phrase.ToLower())
+    {
+        if (Char.IsLetter(c))
+        {
+            letters.Add(c);
+        }
+    }
+    return letters.Count == 26;
+}
